ea5b78f53692b157fb9b7b3d2a59e11e3462bd25
shimantoneer <poddernakla@gmail.com>
2018-05-25 03:14:44 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: http://anthonyrizk.org/services-in-lebanon-middle-east/motivational-speaker-in-lebanon-middle-east/");
+header("Location: http://www.nypintohorse.com/youth-club");
 die();
 ?>
